b1b84796479c14cb906a01b38b46f6c4cfbf2cab
harshil.99 <imharshil03@gmail.com>
2021-01-13 20:13:41 +0530

    -- Streaming fuctionality developed

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import os
 import logging
 from datetime import timedelta
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, Response
 
 from config.accounts import login_service, add_user, get_users, update_user, delete_user
 from config.log_data import get_logs
+from config.streaming import gen_frames, genSecondary_frames, firstCapture, secondCapture
 from data.db import select_query
+import cv2
+
+
 
 module_directory = os.path.dirname(os.path.realpath(__file__))
 
 application = Flask(__name__)
 application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+
+
+camera = cv2.VideoCapture(0)
+camera2 = cv2.VideoCapture(1)
 
 @application.before_request
 def make_session_permanent():
@@ -45,6 +53,48 @@ def dashboard():
         return render_template('dashboard.html', username=session['user_name'], role=session['role'])
     else:
         return render_template('index.html', unauthorized=True)
+
+
+
+
+@application.route('/first_capture', methods=['POST'])
+def first_capture():
+    print('>>>> FRAME CAPTURE FROM FIRST CAMERA <<<<')
+    data = request.form['metricValue']
+    capture = firstCapture()        
+    print('Metric value - ', data)
+    print('First camera frame - ', capture)
+    cv2.imwrite('FirstCamCapture.jpg', capture)
+    return str(capture)
+
+@application.route('/second_capture', methods=['POST'])
+def second_capture():
+    print('>>>> FRAME CAPTURE FROM SECOND CAMERA <<<<')
+    data = request.form['metricValue']
+    capture = secondCapture()        
+    print('Metric value - ', data)
+    print('Second camera frame - ', capture)
+    cv2.imwrite('SecondCamCapture.jpg', capture)
+    return str(capture)
+
+
+
+@application.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@application.route('/secondary_feed')
+def secondary_feed():
+    return Response(genSecondary_frames(camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+@application.route('/test')
+def test():
+    print('Running Test...')
+    return render_template('test.html')
+
+
 
 
 @application.route('/users', methods=['GET', 'PUT', 'DELETE'])
@@ -83,4 +133,4 @@ def logs():
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=5000)
+    application.run(host='127.0.0.1', port=5000)
